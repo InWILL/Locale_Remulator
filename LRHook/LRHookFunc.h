7e5286aba47084a661965415be8fc479b266f5ce
InWILL <InWILL.Phantom@gmail.com>
2025-03-08 19:53:03 +0100

    Update LRHookFunc.h

--- LRHook/LRHookFunc.h
+++ LRHook/LRHookFunc.h
@@ -69,6 +69,18 @@ BOOL WINAPI HookCreateProcessA(
 	_In_ LPSTARTUPINFOA lpStartupInfo,
 	_Out_ LPPROCESS_INFORMATION lpProcessInformation
 );
+BOOL WINAPI HookCreateProcessW(
+	_In_opt_ LPCWSTR lpApplicationName,
+	_Inout_opt_ LPWSTR lpCommandLine,
+	_In_opt_ LPSECURITY_ATTRIBUTES lpProcessAttributes,
+	_In_opt_ LPSECURITY_ATTRIBUTES lpThreadAttributes,
+	_In_ BOOL bInheritHandles,
+	_In_ DWORD dwCreationFlags,
+	_In_opt_ LPVOID lpEnvironment,
+	_In_opt_ LPCWSTR lpCurrentDirectory,
+	_In_ LPSTARTUPINFOW lpStartupInfo,
+	_Out_ LPPROCESS_INFORMATION lpProcessInformation
+);
 
 BOOL WINAPI LRCreateProcessInternalW(
 	_In_opt_ LPCWSTR lpApplicationName,
